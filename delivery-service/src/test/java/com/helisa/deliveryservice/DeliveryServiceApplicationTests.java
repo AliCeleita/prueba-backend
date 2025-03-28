5ffb046ddc79dfdcf328b0302f6db78d299768f1
alice <ali.celeita06@gmail.com>
2025-03-27 21:58:52 -0500

    init

---
+++ delivery-service/src/test/java/com/helisa/deliveryservice/DeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helisa.deliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
